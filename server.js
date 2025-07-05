1f9066569eb9d8b9a311541538e13f4c27a56e4f
masternaip <167619502+masternaip@users.noreply.github.com>
2025-07-06 04:10:25 +0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: '*' }
+});
+
+app.use(cors());
+app.use(express.json());
+
+const MONGO_URI = "mongodb+srv://makie006:vLMtDjOqeSaAm8Vt@hbmaxx.aw7rucy.mongodb.net/?retryWrites=true&w=majority&appName=hbmaxx";
+const DB_NAME = "chatroom";
+const COLLECTION_NAME = "messages";
+
+let messagesCollection;
+
+MongoClient.connect(MONGO_URI, { useUnifiedTopology: true }).then(client => {
+  const db = client.db(DB_NAME);
+  messagesCollection = db.collection(COLLECTION_NAME);
+  server.listen(3000, () => console.log('Server running on port 3000'));
+});
+
+io.on('connection', (socket) => {
+  // Send previous messages
+  messagesCollection.find().sort({ timestamp: 1 }).toArray().then(messages => {
+    socket.emit('messages', messages);
+  });
+
+  socket.on('chat message', (msg) => {
+    const message = { text: msg, timestamp: new Date() };
+    messagesCollection.insertOne(message).then(() => {
+      io.emit('chat message', message);
+    });
+  });
+});
