15e627e1391b9cfdef1cea260c8f1d1ad012e7d7
masternaip <167619502+masternaip@users.noreply.github.com>
2025-07-06 04:24:53 +0800

    Update server.js

--- server.js
+++ server.js
@@ -13,28 +13,49 @@ const io = new Server(server, {
 app.use(cors());
 app.use(express.json());
 
+// Your MongoDB URI
 const MONGO_URI = "mongodb+srv://makie006:vLMtDjOqeSaAm8Vt@hbmaxx.aw7rucy.mongodb.net/?retryWrites=true&w=majority&appName=hbmaxx";
 const DB_NAME = "chatroom";
 const COLLECTION_NAME = "messages";
 
 let messagesCollection;
 
-MongoClient.connect(MONGO_URI, { useUnifiedTopology: true }).then(client => {
-  const db = client.db(DB_NAME);
-  messagesCollection = db.collection(COLLECTION_NAME);
-  server.listen(3000, () => console.log('Server running on port 3000'));
+// Connect to MongoDB
+MongoClient.connect(MONGO_URI, { useUnifiedTopology: true })
+  .then(client => {
+    const db = client.db(DB_NAME);
+    messagesCollection = db.collection(COLLECTION_NAME);
+    server.listen(3000, () => console.log('Server running on http://localhost:3000'));
+  })
+  .catch(err => {
+    console.error('MongoDB connection error:', err);
+    process.exit(1);
+  });
+
+// Socket.io logic
+io.on('connection', (socket) => {
+  // Send previous messages to the newly connected client
+  messagesCollection.find().sort({ timestamp: 1 }).toArray()
+    .then(messages => {
+      socket.emit('messages', messages);
+    })
+    .catch(err => console.error('Error fetching messages:', err));
+
+  // Listen for new chat messages
+  socket.on('chat message', (msg) => {
+    const message = {
+      text: msg,
+      timestamp: new Date()
+    };
+    messagesCollection.insertOne(message)
+      .then(() => {
+        io.emit('chat message', message);
+      })
+      .catch(err => console.error('Error saving message:', err));
+  });
 });
 
-io.on('connection', (socket) => {
-  // Send previous messages
-  messagesCollection.find().sort({ timestamp: 1 }).toArray().then(messages => {
-    socket.emit('messages', messages);
-  });
-
-  socket.on('chat message', (msg) => {
-    const message = { text: msg, timestamp: new Date() };
-    messagesCollection.insertOne(message).then(() => {
-      io.emit('chat message', message);
-    });
-  });
+// (Optional) health check endpoint
+app.get('/', (req, res) => {
+  res.send('Chat server is running!');
 });
